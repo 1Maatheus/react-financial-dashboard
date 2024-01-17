3cdcd02e76e591681fed6a13003165e62f52ca2d
Matheus Oliveira Monteiro <matheusoliveira7227@gmail.com>
2024-01-17 11:49:54 -0300

    adding new components and hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Resumo from "./Pages/Resumo";
+import Sidenav from "./Components/Sidenav";
+import Header from "./Components/Header";
+import "./Style.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Header />
+        <Resumo />
+      </main>
+    </div>
+  );
 }
 
 export default App;
